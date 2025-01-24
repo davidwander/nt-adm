46b6fc7331bef391146d038b7a42cd50bec45dea
David <dwander616@gmail.com>
2025-01-24 12:23:27 -0300

    add fontFamily a component Loading

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,8 +4,24 @@ import { Slot } from "expo-router";
 import  { StatusBar } from "expo-status-bar";
 
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  useFonts, 
+  Poppins_400Regular, 
+  Poppins_500Medium,
+  Poppins_700Bold
+} from "@expo-google-fonts/poppins";
+import { Loading } from "@/components/loading";
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <Loading />
+  }
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar 
